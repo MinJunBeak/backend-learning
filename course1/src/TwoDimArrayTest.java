1c97a4a805371a6239ea7f6341f71309b644d7de
mac_BeakMinJun <beakminjun92@gmail.com>
2024-09-10 06:56:04 +0900

    [add] 2-6-7

---
+++ course1/src/TwoDimArrayTest.java
@@ -0,0 +1,15 @@
+public class TwoDimArrayTest {
+    public static void main(String[] args) {
+        int[][] score = new int[2][4];
+        score[0][0]=10;
+        score[0][1]=10;
+        score[0][2]=10;
+        score[0][3]=10;
+        score[1][0]=10;
+        score[1][1]=10;
+        score[1][2]=10;
+        score[1][3]=10;
+
+        System.out.println(score[0][0]);
+    }
+}
