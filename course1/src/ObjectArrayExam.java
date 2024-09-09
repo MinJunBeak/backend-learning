953f1092e8deebac18cefe8990621cfb643b5e3e
mac_BeakMinJun <beakminjun92@gmail.com>
2024-09-09 17:16:33 +0900

    [add] 2-6-7

--- course1/src/ObjectArrayExam.java
+++ course1/src/ObjectArrayExam.java
@@ -1,14 +1,30 @@
 public class ObjectArrayExam {
     public static void main(String[] args) {
-        Movie[] m = new Movie[3];
-        m[0] = new Movie();
-        m[1] = new Movie();
-        m[2] = new Movie();
+        Movie[] m = {new Movie(), new Movie(), new Movie()};
         m[0].title = "안녕, 할부지";
+        m[0].releaseDate = "2024.09.04";
+        m[0].character = "푸바오";
+        m[0].genre = "다큐멘터리";
+        m[0].runningTime = 95;
+        m[0].rating = "전체 관람가";
+
         m[1].title = "파일럿";
+        m[1].releaseDate = "2024.07.31";
+        m[1].character = "조정석";
+        m[1].genre = "코미디";
+        m[1].runningTime = 111;
+        m[1].rating = "12세 이상 관람가";
+
         m[2].title = "빅토리";
-        System.out.println(m[0].title);
-        System.out.println(m[1].title);
-        System.out.println(m[2].title);
+        m[2].releaseDate = "2024.08.14";
+        m[2].character = "혜리";
+        m[2].genre = "드라마";
+        m[2].runningTime = 120;
+        m[2].rating = "12세 이상 관람가";
+
+        for (int i = 0; i < m.length; i++) {
+            System.out.println(m[i].title + "\t" + m[i].releaseDate + "\t" + m[i].character + "\t"
+            + m[i].genre + "\t" + m[i].runningTime + "\t" + m[i].rating);
+        }
     }
 }
--- course1/src/ObjectArrayTest.java
+++ course1/src/ObjectArrayTest.java
@@ -8,13 +8,24 @@ public class ObjectArrayTest {
         b[0].author = "박매일";
         b[0].page = 500;
         b[0].isbn = "11889900";
-        System.out.println("b[0].title = " + b[0].title);
-        System.out.println("b[0].price = " + b[0].price);
-        System.out.println("b[0].company = " + b[0].company);
-        System.out.println("b[0].author = " + b[0].author);
-        System.out.println("b[0].page = " + b[0].page);
-        System.out.println("b[0].isbn = " + b[0].isbn);
         b[1] = new Book();
+        b[1].title = "오라클";
+        b[1].price = 30000;
+        b[1].company = "한빛";
+        b[1].author = "박매일";
+        b[1].page = 200;
+        b[1].isbn = "11778822";
         b[2] = new Book();
+        b[2].title = "인공지능";
+        b[2].price = 43500;
+        b[2].company = "한빛";
+        b[2].author = "박매일";
+        b[2].page = 800;
+        b[2].isbn = "22405900";
+
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i].title + "\t" + b[i].price + "\t" + b[i].company + "\t" +
+                    b[i].author + "\t" + b[i].page + "\t" + b[i].isbn);
+        }
     }
 }
