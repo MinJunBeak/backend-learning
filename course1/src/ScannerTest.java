ce268b3162f79d66a88aae5336e5e2ca460d0333
mac_BeakMinJun <beakminjun92@gmail.com>
2024-09-12 21:07:34 +0900

    [add] 2-7-1

---
+++ course1/src/ScannerTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("정수를 입력하세요:");
+        int num = scan.nextInt();
+        System.out.println("num = " + num);
+
+        System.out.print("실수를 입력하세요:");
+        float f = scan.nextFloat();
+        System.out.println("f = " + f);
+
+        System.out.print("문자열 입력하세요:");
+        String str = scan.next();
+        System.out.println("str = " + str);
+
+        // 버퍼비우기
+        scan.nextLine();
+
+        System.out.print("문자열 입력하세요:");
+        String str1 = scan.nextLine();
+        System.out.println("str = " + str1);
+    }
+}
